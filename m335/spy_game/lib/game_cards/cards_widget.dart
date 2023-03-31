494a9c5c220e7f03601c3461757233492a6713d3
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-03-31 11:01:03 +0200

    add static card with icon

--- m335/spy_game/lib/game_cards/cards_widget.dart
+++ m335/spy_game/lib/game_cards/cards_widget.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CardsWidget {
   Widget cardWidget() {
-    return Text("hi");
+    return Center(
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Container(
+                            margin: EdgeInsets.all(10),
+                            child: FaIcon(
+                              FontAwesomeIcons.userSecret,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
