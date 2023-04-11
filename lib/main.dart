5553e8168263927441423aa2f8d6d9bce5f8d56d
NiciTheNici <nicolasvontobel@protonmail.ch>
2023-04-11 09:22:27 +0200

    fix Icon crashing app

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
             }
           });
         },
-        child: getCurrentIcon(),
+        child: Icon(getCurrentIcon()),
       ),
     );
   }
